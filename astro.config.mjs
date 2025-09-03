0825eeb5340561af71cfc092512a2f0e56b6124b
Christine Tham <chris.tham@hellotham.com>
2025-09-03 13:31:31 +1000

    v3 - upgrade to latest Astro and Tailwind v4, flowbite v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import alpinejs from '@astrojs/alpinejs'
 import robotsTxt from 'astro-robots-txt'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hmginternational.com',
-  integrations: [sitemap(), tailwind(), alpinejs(), robotsTxt()]
-});
+  integrations: [sitemap(), alpinejs(), robotsTxt()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
