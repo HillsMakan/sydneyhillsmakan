8191cdeda3960f92aa408cf7907d0df08b61ca35
Christine Tham <chris.tham@hellotham.com>
2026-02-02 16:39:55 +1100

    Updated to new astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import sitemap from '@astrojs/sitemap'
 import alpinejs from '@astrojs/alpinejs'
 import robotsTxt from 'astro-robots-txt'
 import tailwindcss from '@tailwindcss/vite'
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hmginternational.com',
-  integrations: [sitemap(), alpinejs(), robotsTxt()],
+  integrations: [icon(),sitemap(), alpinejs(), robotsTxt()],
   vite: {
     plugins: [tailwindcss()]
   }
